52580a9295666547775c1674b6b866066659077e
danyllorobemar <danyllo.robemar@gmail.com>
2023-06-14 02:32:40 -0300

    First commit - project created

---
+++ src/test/java/com/devdan/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdan.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
